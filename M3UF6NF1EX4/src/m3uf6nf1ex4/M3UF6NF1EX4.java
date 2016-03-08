ca11d6352db25c33f9173fc706715de21e124883
smusaran <marcgomez@iesmontsia.org>
2016-03-08 17:41:17 +0100

    Prova Marc

---
+++ M3UF6NF1EX4/src/m3uf6nf1ex4/M3UF6NF1EX4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m3uf6nf1ex4;
+
+/**
+ *
+ * @author alumne
+ */
+public class M3UF6NF1EX4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
