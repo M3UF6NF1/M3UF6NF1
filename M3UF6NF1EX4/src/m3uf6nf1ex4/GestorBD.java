d42ca2cdbadddaac3681ebe3e6efccbcaed29f63
smusaran <alumne@pc-alumne>
2016-03-10 20:46:04 +0100

    gestor base

--- M3UF6NF1EX4/src/m3uf6nf1ex4/GestorBD.java
+++ M3UF6NF1EX4/src/m3uf6nf1ex4/GestorBD.java
@@ -1,1 +1,26 @@
+package m3uf6nf1ex4;
+
+import java.sql.* ;
+
+public class GestorBD {
+    //Carregar el controlador per la BD Apache Derby
+    Class.forName("org.apache.derby.jdbc.ClientDriver");
+    
+    //Establir la connexió
+    String urlBaseDades = "jdbc:derby://localhost:1527/UF6NF1Ex4";
+    String usuari = "alumne";
+    String contrasenya = "alumne";
+    
+    Connection c = DriverManager.getConnection(urlBaseDades , usuari, contrasenya);
+    
+    //Enviar una sentència SQL per recuperar els clients
+    Statement cerca = c.createStatement();
+    ResultSet r = cerca.executeQuery("SELECT * FROM CLIENTS");
+    while (r.next()) {
+        System.out.println("Nom: " + r.getString("NOM") + ", Adreça: " + r.getString("APOSTAL"));
+    }
+    //Tancar la connexió
+    c.close();
+}
+        
 
